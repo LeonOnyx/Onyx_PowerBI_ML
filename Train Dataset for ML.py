45bc4ff9265f2f1b32d33d657ef7a5b89724ba69
LeonOnyx <92385544+LeonOnyx@users.noreply.github.com>
2022-01-28 19:20:29 +0000

    Update Train Dataset for ML.py

--- Train Dataset for ML.py
+++ Train Dataset for ML.py
@@ -21,27 +21,26 @@ df_training = df_training.set_axis(col_names,axis=1)
 # Force the float values of the Occupation attribute to an int datatype
 df_training['Occupation'] = df_training['Occupation'].astype('int')
 
-# Setup the PyCaret AutoML experiment 
+# Setup the ML parameters 
 exp_clf = setup(data = df_training, target = 'Buyer', session_id=5614,
-                categorical_features=['Age', 'MaritalStatus', 'Gender', 'YearlyIncome', 'TotalChildren', 'NumberChildrenAtHome', 'Education',  'HouseOwnerFlag'],
+                categorical_features=['Age', 'MaritalStatus', 'NumberCarsOwned'],
                 ordinal_features={'Occupation' : [0,1,2,3,4]},
                 n_jobs=1, # remove parallelism to work in Power BI
                 silent=True,
                 verbose=False)
 
-# Get the model that performs better in the cross-validation phase.
+# Get the best performing model
 best_model = compare_models(verbose=True)
 
 
 # Save the model in a pkl file for future reuse
-save_model(best_model, r'D:\Onyx Data\sales-model')
+save_model(best_model, r'C:\Users\MrLeo\sales-model')
 
 # Merge the feature test dataframe and target test column in a unique test dataframe
 # and save it in a CSV file for future reuse
 df_test = pd.concat([X_test, y_test], axis=1, ignore_index=True).reset_index(drop=True, inplace=False)
 df_test = df_training.set_axis(col_names,axis=1)
-# %%
-df_test.to_csv(r'D:\Onyx Data\sales-model.csv',
+df_test.to_csv(r'C:\Users\MrLeo\sales-model.csv',
                index=False)
 
 # Get model predictions for the test dataframe
